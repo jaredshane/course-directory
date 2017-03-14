2dd8b67ea2d689f40d40cb0507a4bdf0c90dbb35
Jared Fuller <jaredshane@gmail.com>
2017-03-14 09:25:57 -0500

    changed anchor tags to NavLink tags

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,13 @@ class App extends Component {
         <header>
           <span className="icn-logo"><i className="material-icons">code</i></span>
           <ul className="main-nav">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/teachers">Teachers</a></li>
-            <li><a href="/courses">Courses</a></li>
-          </ul>       
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/teachers">Teachers</NavLink></li>
+            <li><NavLink to="/courses">Courses</NavLink></li>
+          </ul>
         </header>
+        { this.props.children }
       </div>
     );
   }
